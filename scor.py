27d8183db893046cb57c6a0d6eacb58cde12ecbf
SlayZar <vaskind@yandex.ru>
2022-02-15 22:47:46 +0300

    Scor

--- scor.py
+++ scor.py
@@ -75,7 +75,10 @@ else:
     file_buffer = st.file_uploader(label = 'Выберите датасет')
     if file_buffer:
         try:
-            df = pd.read_csv(file_buffer, encoding=None)
+            if '.csv' in file_buffer:
+                df = pd.read_csv(file_buffer, encoding=None)
+            elif '.xls' in file_buffer:
+                df = pd.read_excel(file_buffer)
             if 'id' not in df.columns and df[df['time']==0].shape[0] == 1:
                 df['id'] = 1
         except:
